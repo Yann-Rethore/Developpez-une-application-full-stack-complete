a9b16ef127b079867c7490c51ef7752b7d51a237
Yann-Rethore <yann.rethore.pin@gmail.com>
2025-06-12 04:03:22 +0200

    Ajout de la sécurité
    JwtAuthFilter.java
    JwtUtil.java
    pom.xml
    SecurityConfig.java

---
+++ back/src/main/java/com/openclassrooms/mddapi/security/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.mddapi.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${oc.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${oc.app.jwtExpirationMs}")
+    private long jwtExpirationMs;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
