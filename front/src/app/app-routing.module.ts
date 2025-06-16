8664defabca2a7fe13f53f2b987b3e528543a7db
Yann-Rethore <yann.rethore.pin@gmail.com>
2025-06-17 01:16:22 +0200

    Modifs de fichiers Front pour les fonctionalitées

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { ThemeComponent } from './pages/theme/theme.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { ArticlesAbonnesComponent } from './pages/articles-abonnes/articles-abonnes.component';
+import { ArticleDetailComponent } from './pages/article-detail/article-detail.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
@@ -11,7 +15,11 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'themes', component: ThemeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'article', component: ArticleComponent },
+  { path: 'article/abonnes', component: ArticlesAbonnesComponent }, // Dynamic route for article details
+  { path: 'article/:id', component: ArticleDetailComponent }
 
 ];
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -12,14 +12,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { HeaderComponent } from './shared/header/header.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { ArticlesAbonnesComponent } from './pages/articles-abonnes/articles-abonnes.component';
+import { ArticleDetailComponent } from './pages/article-detail/article-detail.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderPublicComponent } from './shared/header-public/header-public.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ThemeComponent, RegisterComponent, LoginComponent],
+  declarations: [AppComponent, HomeComponent, ThemeComponent, RegisterComponent, LoginComponent, HeaderComponent, ProfileComponent, ArticleComponent, ArticlesAbonnesComponent, ArticleDetailComponent, HeaderPublicComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatIconModule,
     HttpClientModule,
     ReactiveFormsModule
   ],
