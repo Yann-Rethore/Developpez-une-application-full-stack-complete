9cf1071f934fc146b8519ae7f602bb949c807e21
Yann-Rethore <yann.rethore.pin@gmail.com>
2025-07-02 13:19:40 +0200

    ajout et modifications des interceptors et app

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -10,8 +10,8 @@ import { ThemeComponent } from './pages/theme/theme.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ArticleComponent } from './pages/article/article.component';
@@ -19,9 +19,15 @@ import { ArticlesAbonnesComponent } from './pages/articles-abonnes/articles-abon
 import { ArticleDetailComponent } from './pages/article-detail/article-detail.component';
 import { MatIconModule } from '@angular/material/icon';
 import { HeaderPublicComponent } from './shared/header-public/header-public.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ErrorComponent } from './pages/error/error.component';
+
+
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ThemeComponent, RegisterComponent, LoginComponent, HeaderComponent, ProfileComponent, ArticleComponent, ArticlesAbonnesComponent, ArticleDetailComponent, HeaderPublicComponent],
+  declarations: [AppComponent, HomeComponent, ThemeComponent, RegisterComponent, LoginComponent, HeaderComponent, ProfileComponent, ArticleComponent, ArticlesAbonnesComponent, ArticleDetailComponent, HeaderPublicComponent, NotFoundComponent, ErrorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -31,7 +37,9 @@ import { HeaderPublicComponent } from './shared/header-public/header-public.comp
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [ { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [ { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
